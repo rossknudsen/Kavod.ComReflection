7f5052db652010264ee0fb371a544062cd86da95
ross <rossknudsen@gmail.com>
2016-06-14 15:55:30 +1200

    Added visuals to registered library dialog indicating whether file exists or not.

--- src/ComBrowser/ViewModel/LibraryRegistrationViewModel.cs
+++ src/ComBrowser/ViewModel/LibraryRegistrationViewModel.cs
@@ -1,13 +1,19 @@
-﻿using GalaSoft.MvvmLight;
+﻿using System.IO;
+using System.Windows.Media;
+using GalaSoft.MvvmLight;
 using Kavod.ComReflection;
 
 namespace ComBrowser.ViewModel
 {
     public class LibraryRegistrationViewModel : ViewModelBase
     {
+        private static readonly Color DefaultFontColor = Colors.Black;
+        private static readonly Color FileMissingFontColor = Colors.Red;
+
         public LibraryRegistrationViewModel(LibraryRegistration registration)
         {
             LibraryRegistration = registration;
+            FontColor = File.Exists(registration.FilePath) ? DefaultFontColor : FileMissingFontColor;
         }
 
         public string FilePath => LibraryRegistration.FilePath;
@@ -15,5 +21,7 @@ namespace ComBrowser.ViewModel
         public string Name => LibraryRegistration.Name;
 
         public LibraryRegistration LibraryRegistration { get; }
+
+        public Color FontColor { get; private set; }
     }
 }
